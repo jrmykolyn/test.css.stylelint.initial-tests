b87a450d787a2b66349395603ce64bef3f05e7f8
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-05-23 09:34:15 -0400

    chore(stylelint) - Updated file with 'indentation' and 'unit-whitelist' rules.

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-  "extends": "stylelint-config-standard"
+	"extends": "stylelint-config-standard",
+	"rules": {
+		"indentation": "tab",
+		"unit-whitelist": [ "rem", "%" ]
+	}
 }
